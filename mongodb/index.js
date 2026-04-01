24411741bd7808452d1fcfcb776b52052d0cd7c2
ajaykumarakraj <ajaykumarakraj1234@gmail.com>
2026-04-01 17:54:50 +0530

    ok

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,14 +1,29 @@
 import express from "express"
-import { MongoClient } from "mongodb"
-const app=express()
+import { Collection, MongoClient } from "mongodb"
 
-const dbname="test"
-const url="mongodb://localhost:2721"
-
-
+const app = express()
+const dbName = "test"
+const url = "mongodb://localhost:27017"
 
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+const client = new MongoClient(url)
+
+// Connect DB once
+async function connectDB() {
+  await client.connect()
+  console.log("MongoDB Connected ✅")
+ const db=client.db(dbName)
+ const collection=db.collection('company')
+ const result=await collection.find()
+ console.log(result)
+}
+connectDB()
+
+
+app.get("/", (req, res) => {
+  res.send("Hello")
 })
-app.listen(3000)
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000 🚀")
+})

6cd54eb43ea22a6696c111a3f09559ee8ef5ddcc
ajaykumarakraj <ajaykumarakraj1234@gmail.com>
2026-04-01 11:13:55 +0530

    ok

--- template engine/index.js
+++ template engine/index.js
@@ -1,8 +1,27 @@
-import express from "express"
+import express, { urlencoded } from "express"
 const app=express()
-app.get("/",(req,res)=>{
-    res.send("home")
+
+
+app.set('view engine','ejs')
+
+// pass data 
+// app.get("/",(req,res)=>{
+//     res.render("home",{name:"ajay test ejs file",company:"future key"})
+// })
+
+// get data and fetch by form 
+app.use(express.urlencoded({extended:false}))
+
+app.get("/add-user",(req,res)=>{
+res.render('adduser')
 })
+
+app.post("/submit-user",(req,res)=>{
+     
+    console.log(req.body)
+    res.render('submit',req.body)
+})
+
 app.listen(3000,()=>{
     console.log("server run")
 })
